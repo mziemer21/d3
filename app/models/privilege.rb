ddf13c93181035998f95481a6415a5b96a8026ab
LOZORD <ljrudberg@gmail.com>
2014-03-27 16:54:14 -0500

    Added valids for Priv

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -1,2 +1,12 @@
 class Privilege < ActiveRecord::Base
+
+  belongs_to :user
+
+  boolList = [:qDrive, :addSong, :editSong, :deleteSong, :grantPermission, :addUser :edituser, :deleteUser]
+
+  for b in boolList
+          validates b, presence: true
+          default: false
+  end 
+
 end
