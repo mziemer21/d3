c621c5acecd82ac8cee3e263e8a43983276c7bd5
LOZORD <ljrudberg@gmail.com>
2014-03-27 18:01:13 -0500

    Finished first step of privileges. Added defaults for Priv in its migration folder.

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -2,11 +2,17 @@ class Privilege < ActiveRecord::Base
 
   belongs_to :user
 
-  boolList = [:qDrive, :addSong, :editSong, :deleteSong, :grantPermission, :addUser :edituser, :deleteUser]
+  # a false value will validate to false -> see below
+  #validates :qDrive, :addSong, :editSong, :deleteSong, presence: true
+  #validates :grantPermission, :addUser, :editUser, :deleteUser, presence: true
 
-  for b in boolList
-          validates b, presence: true
-          default: false
-  end 
+  validates :qDrive, inclusion: { in: [true,false] }
+  validates :addSong, inclusion: { in: [true,false] }
+  validates :editSong, inclusion: { in: [true,false] }
+  validates :deleteSong, inclusion: { in: [true, false] }
+  validates :grantPermission, inclusion: { in: [true, false }
+  validates :addUser, inclusion: { in: [true,false] }
+  validates :editUser, inclusion: { in: [true,false] }
+  validates :deleteUser, inclusion: { in: [true,false] }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates: :name, presence: true
-  validates: :email, presence: true, uniqueness: true { case_sensitive: false }
-  validates: :privilege, presence: true
-  has_one: :privilege
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true { case_sensitive: false }
+  validates :privilege, presence: true
+  has_one :privilege
 end
