26a814ef2bb918055d3c093eedd1c86d39c93662
LOZORD <ljrudberg@gmail.com>
2014-03-27 20:04:07 -0500

    Fixed small error in Priv, sorry y'all!

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -6,14 +6,14 @@ class Privilege < ActiveRecord::Base
   #validates :qDrive, :addSong, :editSong, :deleteSong, presence: true
   #validates :grantPermission, :addUser, :editUser, :deleteUser, presence: true
 
-  validates_inclusion_of :qDrive, in [true, false]
-  validates_inclusion_of :addSong, in [true, false]
-  validates_inclusion_of :editSong, in [true, false]
-  validates_inclusion_of :deleteSong, in [true, false]
-  validates_inclusion_of :grantPermission, in [true, false]
-  validates_inclusion_of :addUser, in [true, false]
-  validates_inclusion_of :editUser, in [true, false]
-  validates_inclusion_of :deleteUser, in [true ,false]
+  validates_inclusion_of :qDrive, in: [true, false]
+  validates_inclusion_of :addSong, in: [true, false]
+  validates_inclusion_of :editSong, in: [true, false]
+  validates_inclusion_of :deleteSong, in: [true, false]
+  validates_inclusion_of :grantPermission, in: [true, false]
+  validates_inclusion_of :addUser, in: [true, false]
+  validates_inclusion_of :editUser, in: [true, false]
+  validates_inclusion_of :deleteUser, in: [true ,false]
   
   def privilege
     @privilege
