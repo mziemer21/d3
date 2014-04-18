1cc2df4db81868ce37af2f630d6f669ff4f4f279
xinfinn <xinfinn@gmail.com>
2014-04-17 20:50:57 -0500

    add criterion to the api search

--- app/controllers/add_box_controller.rb
+++ app/controllers/add_box_controller.rb
@@ -6,23 +6,43 @@ class AddBoxController < ApplicationController
 
 	def addModal
 		@keyword = params[:keyword]
-		@rt = search(@keyword)
+		@criterion = params[:criterion] #criterion
+		@rt = search(@keyword, @criterion)
 	end
 
-	def search(keyword)
-		url = URI.parse('http://musicbrainz.org/ws/2/artist/?query=' + keyword + '&fmt=json')
+	def search(keyword, criterion)
+		# switch criterion 
+		# refact code
+		case criterion.to_i
+			when 1
+				criterion = "artist"
+				filter = criterion
+				@criterion_show = "Artist"
+			when 2
+				criterion = "release-group"
+				filter = "release-groups"
+				@criterion_show = "Album"
+			else
+				criterion = "recording"
+				filter = criterion
+				@criterion_show = "Track"
+
+		end 
+		keyword_search = keyword.clone
+		keyword_search.gsub! /\s+/, '%20'
+		url = URI.parse('http://musicbrainz.org/ws/2/' + criterion + '/?query=' + keyword_search + '&fmt=json')
 
 		res = Net::HTTP.get_response(url)
 		res_json = res.body
-		res_hash = JSON.parse(res_json)["artist"] #.first["area"]["name"]
+		res_hash = JSON.parse(res_json)[filter] #.first["area"]["name"]
 		array_of_hash = []
 		array_of_string = []
 		if res_hash
-		res_hash.each do |hash|
-			#hash = formart_json(hash)
-			array_of_hash << hash
+			res_hash.each do |hash|
+				#hash = formart_json(hash)
+				array_of_hash << hash
+			end
 		end
-	end
 		return array_of_hash
 	end
 
