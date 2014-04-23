2d7028b15df8e881b813e3514b95c6b05b11ccec
LOZORD <ljrudberg@gmail.com>
2014-04-23 13:10:50 -0500

    Updates to the playlist page

--- app/controllers/playlist_page_controller.rb
+++ app/controllers/playlist_page_controller.rb
@@ -1,4 +1,18 @@
 class PlaylistPageController < ApplicationController
+  #def plists
+  #  @songs = Song.order(:artist)
+  #end
+
   def plists
+    @songs = Song.order(:artist)
+
+    respond_to do |format|
+      format.html
+      # json for all songs specified by query param q
+      format.json { render json: @songs.tokens(params[:q]) }
+    end
   end
+
+
+
 end
