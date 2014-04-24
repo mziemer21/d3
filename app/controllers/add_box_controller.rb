0f1be28e1bf94a55faf3f54f6048a4cb2c433cf5
xinfinn <xinfinn@gmail.com>
2014-04-23 22:06:48 -0500

    format the api search page

--- app/controllers/add_box_controller.rb
+++ app/controllers/add_box_controller.rb
@@ -18,8 +18,8 @@ class AddBoxController < ApplicationController
 				filter = criterion
 				@criterion_show = "Artist"
 			when 2
-				criterion = "release"
-				filter = "releases"
+				criterion = "release-group"
+				filter = "release-groups"
 				@criterion_show = "Album"
 				@album_flag = true
 			else
@@ -35,39 +35,7 @@ class AddBoxController < ApplicationController
 		res = Net::HTTP.get_response(url)
 		res_json = res.body
 		res_hash = JSON.parse(res_json)[filter]
-		array_of_hash = []
-		array_of_string = []
-		if res_hash
-			res_hash.each do |hash|
-				#hash = formart_json(hash)
-				array_of_hash << hash
-			end
-		end
-		return array_of_hash
+		return res_hash
 	end
-
-	
-
-	#TODO 
-	def formart_json(hash)
-		name = hash["name"]
-		id  = hash["id"]
-		
-		if hash["area"] 
-			area = hash["area"]["name"]
-		else
-			area = ""
-		end
-
-		if hash["type"]
-			type = hash["type"]
-		else
-			type = ""
-		end
-		
-		string = name + " "  + id + " " + area + "  " + type
-		return string
-	end
-
 
 end
