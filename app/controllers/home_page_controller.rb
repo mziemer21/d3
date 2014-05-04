b70d5801fc61919367f6ada35824e6432ed16559
LOZORD <ljrudberg@gmail.com>
2014-05-04 01:52:54 -0500

    Update to short list of playlists on home page

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,10 @@
 class HomePageController < ApplicationController
   def home
+    @playlists = User.first.playlists #TESTING
   end
+
+  def newest_plists
+    @playlists.last(5).reverse
+  end
+
 end
